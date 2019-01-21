e0baf444d6cba4423e2d9922cf2657975993a8e9
ROH <latsue@naver.com>
2019-01-21 09:23:11 +0900

    init

---
+++ src/main/java/windows.java
@@ -0,0 +1,20 @@
+import javax.swing.*;
+
+/**
+ * this class is called for make explorer windows.
+ * @author ROH
+ * @version 1.0
+ */
+
+
+public class windows extends JFrame {
+
+
+    public static void main(String[] args)
+    {
+
+
+
+    }
+
+}
